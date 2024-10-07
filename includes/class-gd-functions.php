4e1ac48d28227b9a11001446b64b5f85e3534c4c
kenzodailright <31493774+kenzodailright@users.noreply.github.com>
2024-10-07 19:15:09 +0200

    Update class-gd-functions.php

--- includes/class-gd-functions.php
+++ includes/class-gd-functions.php
@@ -1,7 +1,21 @@
 <?php
 class GD_Functions {
-    public function run() {
-        // Codice iniziale del plugin
+    public static function get_dipendenti() {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'gd_dipendenti';
+        return $wpdb->get_results("SELECT * FROM $table_name");
+    }
+
+    public static function get_dipendente($id) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'gd_dipendenti';
+        return $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE id = %d", $id));
+    }
+
+    public static function update_dipendente($id, $data) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'gd_dipendenti';
+        return $wpdb->update($table_name, $data, array('id' => $id));
     }
 }
 ?>
