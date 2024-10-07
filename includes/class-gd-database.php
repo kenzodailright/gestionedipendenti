3204bd7689eac509ab47ab87c078e3b13ad7ecf7
kenzodailright <31493774+kenzodailright@users.noreply.github.com>
2024-10-08 00:04:42 +0200

    Update class-gd-database.php

--- includes/class-gd-database.php
+++ includes/class-gd-database.php
@@ -6,20 +6,39 @@ class GD_Database {
         global $wpdb;
         
         $charset_collate = $wpdb->get_charset_collate();
-        $table_name = $wpdb->prefix . 'gd_ferie_permessi';
         
-        $sql = "CREATE TABLE $table_name (
+        // Tabella per le ferie e permessi
+        $table_name_ferie = $wpdb->prefix . 'gd_ferie_permessi';
+        
+        $sql_ferie = "CREATE TABLE $table_name_ferie (
             id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
             dipendente_id bigint(20) NOT NULL,
             tipo varchar(50) NOT NULL,
-            data_inizio datetime NOT NULL,
-            data_fine datetime NOT NULL,
+            data_inizio date NOT NULL,
+            data_fine date NOT NULL,
+            ora_inizio time DEFAULT NULL,
+            ora_fine time DEFAULT NULL,
             stato varchar(20) DEFAULT 'in_attesa' NOT NULL,
+            note text DEFAULT NULL,
+            PRIMARY KEY (id)
+        ) $charset_collate;";
+        
+        // Tabella per i dipendenti
+        $table_name_dipendenti = $wpdb->prefix . 'gd_dipendenti';
+        
+        $sql_dipendenti = "CREATE TABLE $table_name_dipendenti (
+            id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+            nome varchar(100) NOT NULL,
+            cognome varchar(100) NOT NULL,
+            email varchar(100) NOT NULL,
+            ruolo varchar(50) NOT NULL,
+            sede varchar(100) DEFAULT NULL,
             PRIMARY KEY (id)
         ) $charset_collate;";
         
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($sql);
+        dbDelta($sql_ferie);
+        dbDelta($sql_dipendenti);
     }
 
     // Funzione per aggiornare le tabelle del database se necessario
