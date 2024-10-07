290e6723753181b3dba738c6c72b93c5cdcbdeaf
kenzodailright <31493774+kenzodailright@users.noreply.github.com>
2024-10-07 22:46:17 +0200

    Update richieste-ferie.php

--- admin/views/richieste-ferie.php
+++ admin/views/richieste-ferie.php
@@ -1,1 +1,21 @@
+<!-- Template HTML per il backend - Richieste di Ferie -->
+<div class="wrap">
+    <h1>Richieste di Ferie e Permessi</h1>
+    <table class="wp-list-table widefat fixed striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Dipendente</th>
+                <th>Tipo</th>
+                <th>Data Inizio</th>
+                <th>Data Fine</th>
+                <th>Stato</th>
+                <th>Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php GD_Admin::list_requests(); ?>
+        </tbody>
+    </table>
+</div>
 
